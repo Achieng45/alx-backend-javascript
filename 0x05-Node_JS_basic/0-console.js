8594b70d1a70a69c798bb9bc3fb21e363e7b0d9c
Achieng45 <stephanieachieng45@gmail.com>
2023-03-19 19:20:22 -0400

    l

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
+ * @author Achieng45  <https://github.com/Achieng45>
  */
 const displayMessage = (msg) => {
   console.log(msg);
